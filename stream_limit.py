2845dd970aaea0a6c947c29c5de821d32d955c57
Manohargajengi77 <118792201+Manohargajengi77@users.noreply.github.com>
2023-09-25 20:24:30 +0530

    Update stream_limit.py

--- stream_limit.py
+++ stream_limit.py
@@ -1,2 +1,4 @@
 import streamlit
-streamlit.title('Im a data Engineer')
+streamlit.title('Big data')
+streamlit.header('what is data Engineering')
+streamlit.text('data engineering means to manage the data and analyze the data')
