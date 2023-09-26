f4ed4d0f55ffc0b21db5d00e94ee170d9cbc0fb7
Manohargajengi77 <118792201+Manohargajengi77@users.noreply.github.com>
2023-09-26 17:00:28 +0530

    Update stream_limit.py
    added breakfast menu and 3 items

--- stream_limit.py
+++ stream_limit.py
@@ -1,4 +1,7 @@
 import streamlit
-streamlit.title('Big data')
-streamlit.header('what is data Engineering')
+streamlit.title('My parents New Healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
